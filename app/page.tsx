3c6c32962d76e18b4b2b12dd0687112a0fd56483
vibeclue <most.possible@mail.ru>
2025-12-15 14:48:42 +0300

    add all assets for globals.css(tailwind), all img assets, fonts, add main metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-const page = () => {
-  return <h1 className="text-3xl text-primary-500 font-black">Privetic</h1>;
-};
+const page = () => (
+  <>
+    <h1 className="h1-bold">Privetic</h1>
+    <h1 className="h1-bold font-space-grotesk">kak delishki</h1>
+  </>
+);
 
 export default page;
