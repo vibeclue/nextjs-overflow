108c1cedeb7052dff4f091b1c13674a67b82c071
vibeclue <most.possible@mail.ru>
2025-12-16 13:48:59 +0300

    add begining navbar + add logo and logo img

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
+import Navbar from "@/components/navigation/navbar";
+
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
@@ -58,6 +60,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
