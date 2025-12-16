0a204c5b68b02aa9cd8d0891db5fc0fc9c4d248c
vibeclue <most.possible@mail.ru>
2025-12-16 14:13:40 +0300

    set up light & dark theme, add switcher

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -19,7 +20,9 @@ const Navbar = () => {
 
       <p>Global Search</p>
 
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
