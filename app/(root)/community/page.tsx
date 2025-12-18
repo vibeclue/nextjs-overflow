2e06b9c8df40b4929920ec4d3597297a6435cfdf
vibeclue <most.possible@mail.ru>
2025-12-18 14:50:17 +0300

    Добавил: Мобильная навигация и ссылки навигации

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Community = () => {
+  return <div className="p-49">Community</div>;
+};
+
+export default Community;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
 import ROUTES from "@/constants/routes";
 
 const page = async () => {
@@ -9,17 +8,6 @@ const page = async () => {
     <>
       <h1 className="h1-bold">Privetic</h1>
       <h1 className="h1-bold font-space-grotesk">kak delishki</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </>
   );
 };
