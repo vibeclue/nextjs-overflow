fd4213b5b6f6d72c50e15e9478d946424eb360bf
vibeclue <most.possible@mail.ru>
2025-12-30 10:44:15 +0300

    create Answer model

---
+++ database/answer.mode.ts
@@ -0,0 +1,24 @@
+import { Schema, models, model, Types } from "mongoose";
+
+export interface IAnswer {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upvotes: number;
+  downvotes: number;
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, red: "Question", required: true },
+    content: { type: String, required: true },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Answer = models?.Answer || model<IAnswer>("Question", AnswerSchema);
+
+export default Answer;
