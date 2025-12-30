7d160bab1b0c60011959ce6b809ec8c15fb91a91
vibeclue <most.possible@mail.ru>
2025-12-30 11:11:47 +0300

    create Tag and TagQuestion models

--- database/answer.mode.ts
+++ database/answer.mode.ts
@@ -19,6 +19,6 @@ const AnswerSchema = new Schema<IAnswer>(
   { timestamps: true }
 );
 
-const Answer = models?.Answer || model<IAnswer>("Question", AnswerSchema);
+const Answer = models?.Answer || model<IAnswer>("Answer", AnswerSchema);
 
 export default Answer;
--- database/template.model.ts
+++ database/template.model.ts
@@ -6,6 +6,6 @@ export interface IModel {
 
 const ModelSchema = new Schema<IModel>({}, { timestamps: true });
 
-const Model = models?.Model || model<IModel>("Question", ModelSchema);
+const Model = models?.Model || model<IModel>("Model", ModelSchema);
 
 export default Model;
