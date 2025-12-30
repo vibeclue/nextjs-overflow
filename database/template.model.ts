5fe075c8b54216c29c4e585111c41ad436e68991
vibeclue <most.possible@mail.ru>
2025-12-30 09:59:10 +0300

    create template and Question model

---
+++ database/template.model.ts
@@ -0,0 +1,11 @@
+import { Schema, models, model } from "mongoose";
+
+export interface IModel {
+  id: string;
+}
+
+const ModelSchema = new Schema<IModel>({}, { timestamps: true });
+
+const Model = models?.Model || model<IModel>("Question", ModelSchema);
+
+export default Model;
